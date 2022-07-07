e1d59a2121f9b1debd7a7e334ee6a4621e82919f
Sirintra-264226 <meiwzaza60@gmail.com>
2022-07-07 17:54:11 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const line = require('@line/bot-sdk')
+const express = require('express')
+const axios = require('axios').default
+const dotenv =require('dotenv')
+
+const env = dotenv.config().parsed
+const app = express()
+
+const lineConfig = {
+    channelAccesToken: env.ACCESS_TOKEN,
+    channelSecret: env.SECRET_TOKEN
+}
+
+app.post('/webhook', line.middlewere(lineConfig), async (req, res) => {
+    try {
+        const events = req.body.events
+        console.log('evemt=>>>>',events)
+        return events.length > 0 ? await events.map(item => handleEvent(item)) : res.status(200).send("OK")
+
+    } catch (error) {
+    res.status(500).end()
+    }
+});
+
+const handleEvent = async (event) => {
+console.log(event)
+}
+
+app. listen(4000, () => {
+    console.log('listening on 4000');
+});
